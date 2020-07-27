d541edeed5d4f8ad03b7488e71cff0e3bd20222e
Justin Winger <justinwinger4@gmail.com>
2020-07-27 11:40:35 -0600

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,23 @@
-// create your App component here
+import React, { Component } from 'react'
+
+export default class App extends Component {
+    state ={
+        peopleInSpace: []
+    }
+    componentDidMount() {
+        fetch('http://api.open-notify.org/astros.json')
+        .then(response => response.json())
+        .then(data => {
+            this.ListeningStateChangedEvent({
+                popleInSpace: data.people
+            })
+        })
+    }
+    render() {
+        return (
+            <div>
+                {this.state.peopleInSpace.map(person => person.name)}
+            </div>
+        )
+    }
+}
